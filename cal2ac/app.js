c917db4bf2bcb4f1785f5dfff2f1625c99f9a6fe
Erik Thorselius <erik.thorslius@gmail.com>
2013-11-20 22:25:19 +0100

    Added calendar to project.

--- cal2ac/app.js
+++ cal2ac/app.js
@@ -6,6 +6,7 @@
 var express = require('express')
   , routes = require('./routes')
   , user = require('./routes/user')
+  , calendar = require('./routes/calendar')
   , http = require('http')
   , path = require('path');
 
@@ -20,7 +21,7 @@ app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
-  app.use(require('stylus').middleware(__dirname + '/public'));
+app.use(require('stylus').middleware(__dirname + '/public'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
@@ -30,6 +31,7 @@ if ('development' == app.get('env')) {
 
 app.get('/', routes.index);
 app.get('/users', user.list);
+app.get('/calendar', calendar.show);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
