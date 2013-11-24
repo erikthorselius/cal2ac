8fa3ad85440392e3cb1ef9697612e676cb01d910
Erik Thorselius <erik.thorslius@gmail.com>
2013-11-24 17:41:33 +0100

    Removed template files.

--- cal2ac/app.js
+++ cal2ac/app.js
@@ -5,7 +5,6 @@
 
 var express = require('express')
   , routes = require('./routes')
-  , user = require('./routes/user')
   , calendar = require('./routes/calendar')
   , http = require('http')
   , path = require('path');
@@ -29,8 +28,6 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
-app.get('/users', user.list);
 app.get('/calendar', calendar.show);
 
 http.createServer(app).listen(app.get('port'), function(){
