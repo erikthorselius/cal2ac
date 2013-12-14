b1ed8faf7c8de76ae8128001b005f23ffd009e80
Erik Thorselius <erik.thorselius@gmail.com>
2013-12-14 08:54:38 +0100

    We schedule command if asked for.

--- cal2ac/app.js
+++ cal2ac/app.js
@@ -3,7 +3,11 @@ var express = require('express')
   , http = require('http')
   , path = require('path')
   , schedule = require('node-schedule')
-  , entrys = require('./routes/entrys');
+  , sha1 = require('sha1')
+  , entrys = require('./routes/entrys')
+  , _ = require('underscore')._
+  , sys = require('sys')
+  , exec = require('child_process').exec;
 var app = express();
 
 // all environments
@@ -17,26 +21,37 @@ app.use(express.methodOverride());
 app.use(app.router);
 app.use(require('stylus').middleware(__dirname + '/public'));
 app.use(express.static(path.join(__dirname, 'public')));
-
 // development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
 app.get('/', calendar.show);
+function eachEntry(entrys) {
+  _.each(entrys, scheduleEntry)
+}
 function scheduleEntry(entry) {
-  console.log(entry[0].startTime.getMonth())
-  var j = schedule.scheduleJob({hour: 21, minute: 1}, function() {
-    console.log(entry);
+  var startId = sha1(entry.startTime)
+  var on = schedule.scheduleJob(startId,new Date(entry.startTime), function(){
+       runCommand("tdtool -n 1"); 
   });
+  var endId = sha1(entry.endTime)
+  var off = schedule.scheduleJob(endId,new Date(entry.endTime), function(){
+       runCommand("tdtool -f 1"); 
+  });
+}
+function runCommand(command) {
+  function puts(error, stdout, stderr) { }
+  exec(command, puts);
 }
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
   var rule = new schedule.RecurrenceRule();
-  rule.second = [0, new schedule.Range(1, 59)];
+  rule.minute = [0, new schedule.Range(1, 59)];
+  //rule.second = [0, new schedule.Range(1, 59)];
   //rule.minute = 55;
-    var j = schedule.scheduleJob(rule, function() {
-    entrys.get(scheduleEntry);
+    var j = schedule.scheduleJob("pollLoop",rule, function() {
+    entrys.get(eachEntry);
   });
 });
 
